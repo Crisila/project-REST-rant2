33cc634ecd0020b40dde618e2e0af56d0ff6a1b0
Crisila <crisilaaban@gmail.com>
2023-11-15 18:47:14 -0800

    second time

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 // The first argument to app.use, /places sets all routes in the places controller relative to /places. This means that /places will be added in front of any other path we define in the controller.
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('home')
 })
 
 // It is important that this route is at the bottom, otherwise it will override your other pages.
