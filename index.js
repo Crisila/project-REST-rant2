fb54e2ae38bf2f25275d0b5e6d27f563db242b62
Crisila <crisilaaban@gmail.com>
2023-11-15 19:42:44 -0800

    Adding places view with mock data

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 // The first argument to app.use, /places sets all routes in the places controller relative to /places. This means that /places will be added in front of any other path we define in the controller.
 
+// ===========================================================
+
 app.get('/', (req, res) => {
     res.render('home')
 })
+
 
 // It is important that this route is at the bottom, otherwise it will override your other pages.
 app.get('*', (req, res) => {
