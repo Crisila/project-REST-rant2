4214dc216cd09b13116ccfcef379eb972f6a55e7
Crisila <crisilaaban@gmail.com>
2023-11-17 13:18:31 -0800

    Jazzed up form page

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'Sushi Rest',
+  city: 'Seattle',
+  state: 'WA',
+  cuisines: 'Japanese',
+  pic: '/images/sushi-rest.jpg'
+}, {
+  name: 'Backyard',
+  city: 'Phoenix',
+  state: 'AZ',
+  cuisines: 'Coffee, Bakery, Brunch',
+  pic: '/images/second-rest.jpg'
+}]
