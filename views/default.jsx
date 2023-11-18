66c24f9da9e3de7372d337c2a4c29cd18219022e
Crisila <crisilaaban@gmail.com>
2023-11-17 21:27:30 -0800

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -6,10 +6,17 @@ function Def(html) {
       <head>
         <title>Title</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-          
+
         </link>
         <link rel="stylesheet" href="/css/style.css" />
       </head>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/places">Places</a></li>
+          <li><a href="/places/new">Add Place</a></li>
+        </ul>
+      </nav>
       <body>{html.children}</body>
     </html>
   );
