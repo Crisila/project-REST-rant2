418ef0e1d393a0bbf09440cf9187880d83f7f293
Crisila <crisilaaban@gmail.com>
2023-11-17 21:39:33 -0800

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -27,5 +27,17 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+router.get('/:id', (req, res) => {
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', {place: places[id]})
+  }
+})
+
 
 module.exports = router;
