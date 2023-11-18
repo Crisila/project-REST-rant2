ee083559c11b297362bda81369d4aa601c61db66
Crisila <crisilaaban@gmail.com>
2023-11-18 10:32:11 -0800

    make edit and delete btns

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -22,6 +22,19 @@ function show(data) {
         </div>
       </main>
 
+      <main>
+        <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+          Edit
+        </a>  
+        <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+          <button type="submit" className="btn btn-danger">
+            Delete
+          </button>
+        </form>     
+
+
+      </main>
+
     </Def>
   )
 }
