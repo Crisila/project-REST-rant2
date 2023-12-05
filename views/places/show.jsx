e309f4659cb41f4c3d0bb90a9674e2e6a8b852ab
Crisila <crisilaaban@gmail.com>
2023-12-05 12:16:24 -0800

    Display stars for rating

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -19,16 +19,20 @@ function show(data) {
     let sumRatings = data.place.comments.reduce((tot, c) => {
       return tot + c.stars
     }, 0);
-    let averageRating = sumRatings / data.place.comments.length
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++) {
+      stars += '⭐'
+    }
     rating = (
       <h3>
-        {Math.round(averageRating)} Stars
+        {stars} Stars
       </h3>
     )
     comments = data.place.comments.map(c => (
       
       <div key={c.id} className="border">
-        <h2 className="rant">{c.rant ? 'Rant! ' : 'Rave! '}</h2>
+        <h2 className="rant">{c.rant ? 'Rant! 🤢' : 'Rave! 🤩'}</h2>
         <h4>{c.content}</h4>
         <h3>
           <strong> - {c.author}</strong>
